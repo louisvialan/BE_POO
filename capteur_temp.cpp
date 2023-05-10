ce86930acc5e532e5fd606360ed84726b599e9b2
louisvialan <98895860+louisvialan@users.noreply.github.com>
2023-05-10 22:49:42 +0200

    Add files via upload

--- capteur_temp.cpp
+++ capteur_temp.cpp
@@ -2,22 +2,27 @@
 #include "capteur_temp.hpp"
 #include "DHT.h"
 #define DHTTYPE DHT11
+#if defined(ARDUINO_ARCH_AVR)
+    #define debug  Serial
+
+#elif defined(ARDUINO_ARCH_SAMD) ||  defined(ARDUINO_ARCH_SAM)
+    #define debug  SerialUSB
+#else
+    #define debug  Serial
+#endif
 
 
-capteurth::capteurth(int PIN){
-  capteur dht(PIN,DHTTYPE); 
-
+capteurth::capteurth(uint8_t PIN): capteur(PIN,DHTTYPE)
+{
+  //DHT capteur(PIN,DHTTYPE); 
 }
 
 
 float capteurth::get_temperature()
 {
   float tempe;
-  tempe=this->capteur.readTemperature(False);
+  tempe=this->capteur.readTemperature(0);
   
-
-
-
   return tempe; 
 };
 
@@ -27,3 +32,14 @@ float capteurth::get_humidite()
   humidity=this->capteur.readHumidity();
   return humidity; 
 };
+void capteurth::setup()
+{
+  
+    debug.begin(115200);
+    //debug.println("DHTxx test!");
+    Wire.begin();
+    /*if using WIO link,must pull up the power pin.*/
+    // pinMode(PIN_GROVE_POWER, OUTPUT);
+    // digitalWrite(PIN_GROVE_POWER, 1);
+    this->capteur.begin();
+}
