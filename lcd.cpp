7a22bedf9db44a00e8d433b50c5985d8459a1876
louisvialan <98895860+louisvialan@users.noreply.github.com>
2023-05-10 22:50:32 +0200

    Add files via upload

--- lcd.cpp
+++ lcd.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
-#include <lcd.hpp>
-#include <rgb_lcd.hpp>
+#include "lcd.hpp"
+#include "rgb_lcd.h"
+#include <string>
+using namespace std;
+lcd::lcd():ecran(){
 
+}
 
-void lcd:write()
+void lcd::write(const char* S)
 {
-  setColorWhite();
-  
+  this->ecran.print(S);
+}
+void lcd::setup()
+{
+    this->ecran.begin(16, 2);
+    this->ecran.setColorWhite();
+}
+void lcd::clear()
+{
+  this ->ecran.clear();
+}
+void lcd:: cursor(int a, int b)
+{
+  this ->ecran.setCursor(a,b);
 }
