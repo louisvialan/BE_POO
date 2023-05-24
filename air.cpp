2712e86d6d1e90106a5f4d15b4385ba92342ce52
louisvialan <98895860+louisvialan@users.noreply.github.com>
2023-05-24 09:37:29 +0200

    Add files via upload

--- air.cpp
+++ air.cpp
@@ -3,44 +3,49 @@
 #include <string>
 using namespace std;
 
-
-air::air(int PIN):pollution(PIN)
+air::air(int PIN) : pollution(PIN)
 {
-
+    // Constructeur de la classe air, initialise le capteur de pollution
+    // avec le numéro de PIN fourni.
 }
+
 int air::getValue()
 {
-  int valeur;
-  valeur=this->pollution.getValue();
-  
-  return valeur; 
+    // Récupère la valeur de pollution mesurée par le capteur.
+    int valeur;
+    valeur = this->pollution.getValue();
+    return valeur;
 }
 
 string air::pollutionlevel(int quality)
 {
-  string text; 
-  
+    // Détermine le niveau de pollution en fonction de la qualité de l'air mesurée.
+    // Retourne une chaîne de caractères descriptive du niveau de pollution.
+    string text;
 
     if (quality == AirQualitySensor::FORCE_SIGNAL) {
-        text="ALERTE! POLLUTION MAXIMALE!";
+        text = "ALERTE ! POLLUTION MAXIMALE !";
     } else if (quality == AirQualitySensor::HIGH_POLLUTION) {
-        text="Pollution haute!";
+        text = "Pollution élevée !";
     } else if (quality == AirQualitySensor::LOW_POLLUTION) {
-        text="Pollution faible";
+        text = "Pollution faible.";
     } else if (quality == AirQualitySensor::FRESH_AIR) {
-         text="Air agréable";
+         text = "Air agréable.";
     }
  
- return text; 
+    return text;
 }
 
 int air::pente()
 {
-  int val; 
-  val=this->pollution.slope(); 
-  return val; 
+    // Calcule la pente de variation de la pollution mesurée.
+    int val;
+    val = this->pollution.slope();
+    return val;
 }
+
 /*void air::setup()
 {
-  this->pollution.begin(9600);
+    // Configuration initiale du capteur de pollution.
+    this->pollution.begin(9600);
 }*/
