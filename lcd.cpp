4f3d7c353bdd216f3f412a2a5b331e5ef6de5c73
louisvialan <98895860+louisvialan@users.noreply.github.com>
2023-05-24 09:38:06 +0200

    Add files via upload

--- lcd.cpp
+++ lcd.cpp
@@ -3,36 +3,47 @@
 #include "rgb_lcd.h"
 #include <string>
 using namespace std;
-lcd::lcd():ecran(){
 
+lcd::lcd() : ecran()
+{
+    // Constructeur de la classe lcd, initialise l'objet ecran
+    // de la classe rgb_lcd.
 }
 
 void lcd::write(const char* S)
 {
-  this->ecran.print(S);
+    // Affiche le texte sur l'écran LCD.
+    this->ecran.print(S);
 }
+
 void lcd::setup()
 {
-    this->ecran.begin(16, 2);
-    this->ecran.setColorWhite();
+    // Configuration initiale de l'écran LCD.
+    this->ecran.begin(16, 2);  // Initialise l'écran avec 16 colonnes et 2 lignes
+    this->ecran.setColorWhite();  // Définit la couleur du texte en blanc
 }
+
 void lcd::clear()
 {
-  this ->ecran.clear();
+    // Efface le contenu de l'écran LCD.
+    this->ecran.clear();
 }
-void lcd:: cursor(int a, int b)
+
+void lcd::cursor(int a, int b)
 {
-  this ->ecran.setCursor(a,b);
+    // Positionne le curseur de l'écran LCD à la colonne 'a' et à la ligne 'b'.
+    this->ecran.setCursor(a, b);
 }
-void lcd :: couleur(int temp)
+
+void lcd::couleur(int temp)
 {
-  if (temp>= 30)
-  {
-    this-> ecran.setRGB(255,0,0);
-  }
-  else
-  {
-    this->ecran.setRGB(0,0,255);
-  }
-    
+    // Change la couleur de fond de l'écran LCD en fonction de la température.
+    if (temp >= 30)
+    {
+        this->ecran.setRGB(255, 0, 0);  // Rouge
+    }
+    else
+    {
+        this->ecran.setRGB(0, 0, 255);  // Bleu
+    }
 }
