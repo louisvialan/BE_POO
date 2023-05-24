bc18fb9f6d43d37521accded291c04920fde123a
louisvialan <98895860+louisvialan@users.noreply.github.com>
2023-05-24 09:37:46 +0200

    Add files via upload

--- capteur_temp.cpp
+++ capteur_temp.cpp
@@ -1,45 +1,44 @@
 #include <iostream>
 #include "capteur_temp.hpp"
 #include "DHT.h"
-#define DHTTYPE DHT11
-#if defined(ARDUINO_ARCH_AVR)
-    #define debug  Serial
 
+#define DHTTYPE DHT11
+
+#if defined(ARDUINO_ARCH_AVR)
+    #define DEBUG_SERIAL  Serial
 #elif defined(ARDUINO_ARCH_SAMD) ||  defined(ARDUINO_ARCH_SAM)
-    #define debug  SerialUSB
+    #define DEBUG_SERIAL  SerialUSB
 #else
-    #define debug  Serial
+    #define DEBUG_SERIAL  Serial
 #endif
 
-
-capteurth::capteurth(uint8_t PIN): capteur(PIN,DHTTYPE)
+capteurth::capteurth(uint8_t PIN) : capteur(PIN, DHTTYPE)
 {
-  //DHT capteur(PIN,DHTTYPE); 
+    // Constructeur de la classe capteurth, initialise le capteur DHT
+    // avec le numéro de PIN fourni.
+    // DHT capteur(PIN, DHTTYPE);
 }
-
 
 float capteurth::get_temperature()
 {
-  float tempe;
-  tempe=this->capteur.readTemperature(0);
-  
-  return tempe; 
-};
+    // Récupère la température du capteur DHT.
+    float temperature;
+    temperature = this->capteur.readTemperature(0);
+    return temperature;
+}
 
 float capteurth::get_humidite()
 {
-  float humidity;
-  humidity=this->capteur.readHumidity();
-  return humidity; 
-};
+    // Récupère l'humidité du capteur DHT.
+    float humidity;
+    humidity = this->capteur.readHumidity();
+    return humidity;
+}
+
 void capteurth::setup()
 {
-  
-    debug.begin(115200);
-    //debug.println("DHTxx test!");
-    Wire.begin();
-    /*if using WIO link,must pull up the power pin.*/
-    // pinMode(PIN_GROVE_POWER, OUTPUT);
-    // digitalWrite(PIN_GROVE_POWER, 1);
-    this->capteur.begin();
+    // Configuration initiale du capteur DHT.
+    DEBUG_SERIAL.begin(115200);  // Initialise la communication série pour le débogage
+    Wire.begin();  // Initialise la communication I2C
+    this->capteur.begin();  // Initialise le capteur DHT
 }
