90982338ed053ea4b0f9e0bf44d98c310386089b
louisvialan <98895860+louisvialan@users.noreply.github.com>
2023-05-24 09:38:43 +0200

    Add files via upload

--- wifi.cpp
+++ wifi.cpp
@@ -1,22 +1,29 @@
 #include "wifi.hpp"
 
+
 coWifi::coWifi(const char* ssid, const char* mdp) {
-  this->ssid = ssid;
-  this->mdp = mdp;
+    // Constructeur de la classe coWifi, initialise les informations du réseau WiFi.
+    this->ssid = ssid;
+    this->mdp = mdp;
 }
 
 void coWifi::connect() {
-  Serial.print("Tentative de connexion au réseau WiFi ");
-  Serial.println(ssid);
+    // Connecte l'ESP8266 au réseau WiFi spécifié.
 
-  WiFi.begin(ssid, mdp);
+    // Affiche le SSID du réseau WiFi auquel on se connecte
+    Serial.print("Tentative de connexion au réseau WiFi ");
+    Serial.println(ssid);
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
+    // Démarre la connexion au réseau WiFi
+    WiFi.begin(ssid, mdp);
 
-  Serial.println("");
-  Serial.println("WiFi connecté");
-  Serial.println("Adresse IP : " + WiFi.localIP().toString());
+    // Attends que la connexion soit établie
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connecté");
+    Serial.println("Adresse IP : " + WiFi.localIP().toString());
 }
